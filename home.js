ea4588b4be60dccda6a11fc36edd5b37c4503de0
shashvindu <56150644+shashvindu@users.noreply.github.com>
2024-09-25 22:50:31 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,20 @@
+function navigateTo(field) {
+    let url = '';
+    switch (field) {
+        case 'data-science':
+            url = 'data-science.html';
+            break;
+        case 'data-engineering':
+            url = 'data-engineering.html';
+            break;
+        case 'data-analyst':
+            url = 'data-analyst.html';
+            break;
+        case 'math-stats':
+            url = 'math-statistics.html';
+            break;
+        default:
+            url = 'home.html';
+    }
+    window.location.href = url;
+}
